1a91192489cc768bb317276ea041dd9946e5f80f
mishrakash <mishrakash007@gmail.com>
2024-07-30 20:46:08 +0530

    End of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+
+  //constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5227/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
+  }
+}
