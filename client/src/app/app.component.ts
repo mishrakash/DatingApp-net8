7e0f6318d0ee90e713d0853dc33035fcfadbdf40
mishrakash <mishrakash007@gmail.com>
2024-08-05 16:35:14 +0530

    End of Section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,26 +2,31 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
+  private accountService = inject(AccountService);
   title = 'DatingApp';
-  users: any;
 
   //constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5227/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 }
